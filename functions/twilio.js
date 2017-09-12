42bdb97ac30db7b4d003383279b2e11fc4678022
davidmgudeman <davidmgudeman@gmail.com>
2017-09-12 08:03:37 -0700

    starting the twilio texting process

---
+++ functions/twilio.js
@@ -0,0 +1,7 @@
+
+const twilio = require('twilio');
+
+const accountSid = 'AC171799aca43f6957f29092d4341f0c2b';
+const authToken = '927800454db7767a2433699dc7332d47';
+
+module.exports = new twilio.Twilio(accountSid, authToken);
