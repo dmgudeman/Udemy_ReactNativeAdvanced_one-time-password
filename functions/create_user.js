cec98b72f0426fbe84a6c91129e70b1e10f74883
davidmgudeman <davidmgudeman@gmail.com>
2017-09-12 16:16:17 -0700

    the verify works now

--- functions/create_user.js
+++ functions/create_user.js
@@ -7,13 +7,12 @@ module.exports = function(req, res) {
   }
 
   // Format the phone number to remove dashes and parens
-     // Make sure the input is a string with the String constructor
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
   // Create a new user account using that phone number
   admin.auth().createUser({ uid: phone })
     .then(user => res.send(user))
     .catch(err => res.status(422).send({ error: err }));
-  // Respond to the user request, saying the account was made
 
+  // Respond to the user request, saying the account was made
 }
